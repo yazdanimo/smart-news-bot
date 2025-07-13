2d050ea6eb0c9f387c1db4810fbe8d8209be54a5
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-14 01:56:43 +0330

    Update db.py

--- db.py
+++ db.py
@@ -10,10 +10,10 @@ c.execute("""
 """)
 conn.commit()
 
-def is_duplicate(text):
-    c.execute("SELECT text FROM messages WHERE text=?", (text,))
+def is_duplicate(text: str) -> bool:
+    c.execute("SELECT 1 FROM messages WHERE text=?", (text,))
     return c.fetchone() is not None
 
-def save_message(text):
+def save_message(text: str):
     c.execute("INSERT INTO messages (text) VALUES (?)", (text,))
     conn.commit()
