d186fedeaf600c2c5ed4171c7ec74497b5aab88f
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-14 18:20:27 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
 
+import os
+from dotenv import load_dotenv
+
+# بارگذاری متغیرهای محیطی
+load_dotenv()
+
+BOT_TOKEN   = os.getenv("BOT_TOKEN")
+CHANNEL_ID  = int(os.getenv("CHANNEL_ID", 0))
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "").rstrip("/")
+PORT        = int(os.getenv("PORT", 8080))
+
+if not all([BOT_TOKEN, CHANNEL_ID, WEBHOOK_URL]):
+    raise RuntimeError("Environment variables BOT_TOKEN, CHANNEL_ID or WEBHOOK_URL missing")
