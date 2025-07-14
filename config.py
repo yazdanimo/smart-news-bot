fb4bb79752af346155aad6e7e2df6af231176e79
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-14 20:13:15 +0330

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL", "").rstrip("/")
 PORT        = int(os.getenv("PORT", 8080))
 
 if not all([BOT_TOKEN, CHANNEL_ID, WEBHOOK_URL]):
-    raise RuntimeError("Environment variables BOT_TOKEN, CHANNEL_ID or WEBHOOK_URL missing")
+    raise RuntimeError("Missing one of BOT_TOKEN, CHANNEL_ID or WEBHOOK_URL")
