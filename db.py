d1f399880366b3a80de76cd05ac64833d1f7353a
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-14 21:10:46 +0330

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect("messages.db", check_same_thread=False)
 cursor = conn.cursor()
-
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS messages (
         text TEXT PRIMARY KEY
