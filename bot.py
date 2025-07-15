5e66e67123d1fd10ac67e3730dd786e08aa60dad
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-15 21:36:39 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 import os
 from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import Dispatcher
+from telegram.ext import Dispatcher, MessageHandler, Filters
 from config import TOKEN, BASE_URL, PORT
 from handlers import news_handler
 from db import init_db
@@ -19,7 +19,7 @@ app = Flask(__name__)
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, update_queue=None, workers=4, use_context=True)
 
-# ثبت handler برای همه‌ی پیام‌های متنی
+# ثبت handler برای همه‌ی پیام‌های متنی (غیر از کامندها)
 dp.add_handler(MessageHandler(Filters.text & ~Filters.command, news_handler))
 
 # مسیر وب‌هوک باید دقیقاً /<TOKEN> باشد
@@ -27,6 +27,9 @@ WEBHOOK_PATH = f"/{TOKEN}"
 
 @app.route(WEBHOOK_PATH, methods=["POST"])
 def webhook_handler():
+    """
+    این endpoint داده‌ی آپدیت تلگرام را دریافت و پردازش می‌کند.
+    """
     data = request.get_json(force=True)
     update = Update.de_json(data, bot)
     dp.process_update(update)
