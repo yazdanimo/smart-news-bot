4d16f46e95fd401d1f957e482877fcf22a24d3ad
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-15 18:11:44 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 BOT_TOKEN = "7605984218:AAGRWqGvr5tWuT_uPaqbm5gzBa7mBhIMJn4"
 CHANNEL_ID = -1002685190359
-MODE = "webhook"                       # polling یا webhook
-WEBHOOK_URL = "https://YOUR-RAILWAY-URL"  # آدرس اپلیکیشن Railway
+MODE = "webhook"
+WEBHOOK_URL = "https://web-production-259f1.up.railway.app"
 PORT = "8080"
-DB_PATH = "/tmp/messages.db"          # در Railway از /tmp استفاده کن
+DB_PATH = "/tmp/messages.db"
