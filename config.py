efe1450ed930932f040952f26fc55930ac4104b5
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-15 22:29:00 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
+# config.py
 import os
 
-# این متغیرها حتما باید در محیط (Railway) ست شده باشند
 BOT_TOKEN   = os.environ["BOT_TOKEN"]
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 CHANNEL_ID  = os.environ["CHANNEL_ID"]
 DB_PATH     = os.environ["DB_PATH"]
-MODE        = os.environ.get("MODE", "webhook")   # در صورت عدم تنظیم، webhook می‌ماند
-PORT        = int(os.environ.get("PORT", "8443"))  # پیش‌فرض 8443
+MODE        = os.environ.get("MODE", "webhook")
+PORT        = int(os.environ.get("PORT", "8443"))
